a0da0285e1a4d22ee27f4ba48e7dce6779de26fe
Lawrence Norton <lawrence.norton@akqa.com>
2016-05-03 17:50:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-webpack-test [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> a simple webpack
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-webpack-test using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-webpack-test
+```
+
+Then generate your new project:
+
+```bash
+yo webpack-test
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Lawrence Norton]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-webpack-test.svg
+[npm-url]: https://npmjs.org/package/generator-webpack-test
+[travis-image]: https://travis-ci.org/lawrence79/generator-webpack-test.svg?branch=master
+[travis-url]: https://travis-ci.org/lawrence79/generator-webpack-test
+[daviddm-image]: https://david-dm.org/lawrence79/generator-webpack-test.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/lawrence79/generator-webpack-test
---
+++ app/index.js
@@ -0,0 +1,117 @@
+'use strict';
+var file = require('file');
+var path = require('path');
+var generators = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var mkdirp = require('mkdirp');
+var camelcase = require('lodash.camelcase');
+var kebabcase = require('lodash.kebabcase');
+var trim = require('lodash.trim');
+var jsesc = require('jsesc');
+var Promise = require('bluebird');
+var exec = Promise.promisify(require('child_process').exec);
+var gitConfig = require('git-config');
+
+function jsonEscape(str) {
+  return jsesc(str, {quotes: 'double'});
+}
+
+module.exports = generators.Base.extend({
+  initializing: function() {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the ' + chalk.red('Babel Library Boilerplate') + ' generator!'
+    ));
+
+    return Promise.all([exec('npm whoami').catch(function(e) {
+      console.error('Error getting npm user name: run `npm login`');
+      console.error(e);
+    })])
+    .then(function(result) {
+      result = result ? result : {};
+      this.username = trim(result[0]);
+      return this._showPrompts();
+    }.bind(this));
+  },
+
+  _showPrompts: function() {
+    var config = gitConfig.sync();
+    config.user = config.user ? config.user : {};
+    var prompts = [{
+      type: 'input',
+      name: 'user',
+      message: 'What is the Github username/organization for this project?',
+      default: this.username,
+      store: true
+    }, {
+      type: 'input',
+      name: 'repo',
+      message: 'What is the repository/project name?',
+      default: kebabcase(this.appname)
+    }, {
+      type: 'input',
+      name: 'description',
+      message: 'What is a short description for this project?'
+    }, {
+      type: 'input',
+      name: 'author',
+      message: 'Who is the author of this project?',
+      default: config.user.name + ' <' + config.user.email + '>',
+      store: true
+    }, {
+      type: 'input',
+      name: 'variable',
+      message: 'If there is one, what is the name of this project\'s main variable?',
+      default: camelcase(this.appname)
+    }];
+
+    var self = this;
+    return new Promise(function(resolve, reject) {
+      self.prompt(prompts, function(props) {
+        self.user = jsonEscape(props.user);
+        self.repo = jsonEscape(props.repo);
+        self.description = jsonEscape(props.description);
+        self.author = jsonEscape(props.author);
+        self.variable = props.variable;
+        resolve();
+      });
+    });
+  },
+
+  writing: {
+    app: function() {
+      var src = this.sourceRoot();
+      var self = this;
+      file.walkSync(src, function(dirPath, dirs, files) {
+        var relativeDir = path.relative(src, dirPath);
+        files.forEach(function(filename) {
+          var target;
+          // Only copy the files that we don't want to rename. We do that after this loop.
+          // The files we don't want to rename are both "index.js", and one of them is in
+          // "test/unit," and the other is in "src"
+          var ignoreDir = relativeDir === 'test/unit' || relativeDir === 'src';
+          var shouldCopy = !ignoreDir && !/index.js$/.test(filename);
+          if (shouldCopy) {
+            target = path.join(relativeDir, filename);
+            self.template(target, target);
+          }
+        });
+      });
+      this.template('src/index.js', 'src/' + this.repo + '.js');
+      this.template('test/unit/index.js', 'test/unit/' + this.repo + '.js');
+    }
+  },
+
+  install: function() {
+    this.installDependencies({
+      bower: false,
+      npm: true,
+      skipInstall: this.options['skip-install']
+    });
+  }
+});
