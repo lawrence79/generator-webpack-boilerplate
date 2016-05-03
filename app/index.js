783ae0f782b16eb22f7cd1edc0b0372d85969193
Lawrence Norton <lawrence.norton@akqa.com>
2016-05-03 18:10:21 -0400

    switching out for webpack with config file

--- app/index.js
+++ app/index.js
@@ -1,117 +1,76 @@
 'use strict';
-var file = require('file');
-var path = require('path');
-var generators = require('yeoman-generator');
+var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
-var mkdirp = require('mkdirp');
-var camelcase = require('lodash.camelcase');
-var kebabcase = require('lodash.kebabcase');
-var trim = require('lodash.trim');
-var jsesc = require('jsesc');
-var Promise = require('bluebird');
-var exec = Promise.promisify(require('child_process').exec);
-var gitConfig = require('git-config');
 
-function jsonEscape(str) {
-  return jsesc(str, {quotes: 'double'});
-}
-
-module.exports = generators.Base.extend({
-  initializing: function() {
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
     this.pkg = require('../package.json');
   },
 
-  prompting: function() {
+  prompting: function () {
+    var done = this.async();
+
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the ' + chalk.red('Babel Library Boilerplate') + ' generator!'
+      'Welcome to the ' + chalk.red('webpack-ws6-cssnext') + ' generator!'
     ));
 
-    return Promise.all([exec('npm whoami').catch(function(e) {
-      console.error('Error getting npm user name: run `npm login`');
-      console.error(e);
-    })])
-    .then(function(result) {
-      result = result ? result : {};
-      this.username = trim(result[0]);
-      return this._showPrompts();
+    var prompts = [{
+      type    : 'input',
+      name    : 'projectName',
+      message : 'Project name',
+      default : this.appname // Default to current folder name
+    },
+    {
+      type    : 'input',
+      name    : 'projectDescription',
+      message : 'Project description'
+    },
+    {
+      type    : 'input',
+      name    : 'authorName',
+      message : 'Author name'
+    },
+    {
+      type    : 'input',
+      name    : 'authorEmail',
+      message : 'Author email'
+    },
+    {
+      type    : 'input',
+      name    : 'license',
+      message : 'License'
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      done();
     }.bind(this));
   },
 
-  _showPrompts: function() {
-    var config = gitConfig.sync();
-    config.user = config.user ? config.user : {};
-    var prompts = [{
-      type: 'input',
-      name: 'user',
-      message: 'What is the Github username/organization for this project?',
-      default: this.username,
-      store: true
-    }, {
-      type: 'input',
-      name: 'repo',
-      message: 'What is the repository/project name?',
-      default: kebabcase(this.appname)
-    }, {
-      type: 'input',
-      name: 'description',
-      message: 'What is a short description for this project?'
-    }, {
-      type: 'input',
-      name: 'author',
-      message: 'Who is the author of this project?',
-      default: config.user.name + ' <' + config.user.email + '>',
-      store: true
-    }, {
-      type: 'input',
-      name: 'variable',
-      message: 'If there is one, what is the name of this project\'s main variable?',
-      default: camelcase(this.appname)
-    }];
-
-    var self = this;
-    return new Promise(function(resolve, reject) {
-      self.prompt(prompts, function(props) {
-        self.user = jsonEscape(props.user);
-        self.repo = jsonEscape(props.repo);
-        self.description = jsonEscape(props.description);
-        self.author = jsonEscape(props.author);
-        self.variable = props.variable;
-        resolve();
-      });
-    });
-  },
-
   writing: {
-    app: function() {
-      var src = this.sourceRoot();
-      var self = this;
-      file.walkSync(src, function(dirPath, dirs, files) {
-        var relativeDir = path.relative(src, dirPath);
-        files.forEach(function(filename) {
-          var target;
-          // Only copy the files that we don't want to rename. We do that after this loop.
-          // The files we don't want to rename are both "index.js", and one of them is in
-          // "test/unit," and the other is in "src"
-          var ignoreDir = relativeDir === 'test/unit' || relativeDir === 'src';
-          var shouldCopy = !ignoreDir && !/index.js$/.test(filename);
-          if (shouldCopy) {
-            target = path.join(relativeDir, filename);
-            self.template(target, target);
-          }
-        });
-      });
-      this.template('src/index.js', 'src/' + this.repo + '.js');
-      this.template('test/unit/index.js', 'test/unit/' + this.repo + '.js');
+    app: function () {
+      [
+        {src: '_package.json', dst: 'package.json'},
+        {src: 'webpack.config.js', dst: 'webpack.config.js'},
+        {src: 'index.html', dst: 'index.html'},
+        {src: 'src/css/main.css', dst: 'src/css/main.css'},
+        {src: 'src/js/app.js', dst: 'src/js/app.js'}
+      ].forEach(function (f) {
+        this.fs.copyTpl(this.templatePath(f.src), this.destinationPath(f.dst), this.props);
+      }.bind(this));
+    },
+
+    projectfiles: function () {
+      var dotfiles = ['editorconfig', 'jshintrc', 'jscsrc', 'gitignore'];
+      dotfiles.forEach(function (dotfile) {
+        this.fs.copy(this.templatePath(dotfile), this.destinationPath('.' + dotfile));
+      }.bind(this));
     }
   },
 
-  install: function() {
-    this.installDependencies({
-      bower: false,
-      npm: true,
-      skipInstall: this.options['skip-install']
-    });
+  install: function () {
+    this.installDependencies();
   }
 });
